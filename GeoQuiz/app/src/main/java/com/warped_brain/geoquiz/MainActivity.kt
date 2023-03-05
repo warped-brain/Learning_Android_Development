27b42e0398b5091665f1d929fa8484eeefd84760
warped-brain <warped-brain@users.noreply.github.com>
2023-03-05 11:52:44 +0530

    Chapter 1 Completed.

--- GeoQuiz/app/src/main/java/com/warped_brain/geoquiz/MainActivity.kt
+++ GeoQuiz/app/src/main/java/com/warped_brain/geoquiz/MainActivity.kt
@@ -2,10 +2,42 @@ package com.warped_brain.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton : Button
+    private lateinit var falseButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        trueButton.setOnClickListener { view: View ->
+//            Toast.makeText(
+//                this,
+//                R.string.correct_toast,
+//                Toast.LENGTH_SHORT
+//            ).show()
+            Snackbar.make(
+                view,
+                R.string.correct_toast,
+                4000
+            ).show()
+        }
+        falseButton.setOnClickListener {view: View ->
+//            Toast.makeText(
+//                this,
+//                R.string.incorrect_toast,
+//                Toast.LENGTH_SHORT
+//            ).show()
+            Snackbar.make(
+                view,
+                R.string.incorrect_toast,
+                1000
+            ).show()
+        }
     }
 }
